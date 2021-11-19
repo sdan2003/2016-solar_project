2f605482164c30388fad46842fd755c8d16e1e76
Surinov Daniil <surinov.de@phystech.edu>
2021-11-19 09:36:35 +0300

    Вроде более-менее работает двойная звезда

--- solar_input.py
+++ solar_input.py
@@ -14,15 +14,19 @@ def read_space_objects_data_from_file(input_filename):
     """
 
     objects = []
-    with open(input_filename) as input_file:
+    with open(input_filename, encoding='utf8') as input_file:
         for line in input_file:
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":  ### FIXME: do the same for planet
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == 'planet':
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -43,8 +47,16 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
-
-    pass  # FIXME: not done yet
+    list_of_data = line.split()
+    star.R = float(list_of_data[1])
+    star.color = list_of_data[2]
+    star.m = float(list_of_data[3])
+    star.x = float(list_of_data[4])
+    star.y = float(list_of_data[5])
+    star.Vx = float(list_of_data[6])
+    star.Vy = float(list_of_data[7])
+    return star
+    ### FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +73,16 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    list_of_data = line.split()
+    planet.R = float(list_of_data[1])
+    planet.color = list_of_data[2]
+    planet.m = float(list_of_data[3])
+    planet.x = float(list_of_data[4])
+    planet.y = float(list_of_data[5])
+    planet.Vx = float(list_of_data[6])
+    planet.Vy = float(list_of_data[7])
+    return planet
+    ### FIXME: not done yet...
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
@@ -77,10 +98,16 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+            if obj.type == 'Star':
+                print(out_file, 'Star ' + obj.R + ' ' + obj.color + ' ' + obj.m +
+                    ' ' + obj.x + ' ' + obj.y + ' ' + obj.Vx + ' ' + obj.Vy)
+            elif obj.type == 'Planet':
+                print(out_file, 'Planet ' + str(obj.R) + ' ' + str(obj.color) + ' ' + str(obj.m) +
+                    ' ' + str(obj.x) + ' ' + str(obj.y) + ' ' + str(obj.Vx) + ' ' + str(obj.Vy))
+            ### FIXME: should store real values
+            # Надо посмотреть переход на следующую строку
 
-# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+# FIXME: хорошо бы ещё сделать функцию, сохраняющую статистику в заданный файл...
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
